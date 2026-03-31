f74c6124cc47a320430066cc9f73871cb2e161f1
Vedant Gonbare <123235269+vedantgonbare@users.noreply.github.com>
2026-03-31 10:52:13 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
 const api = axios.create({
   baseURL: API_BASE,
